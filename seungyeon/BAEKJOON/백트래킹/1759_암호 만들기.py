c384f7a2f0cdbdbb426dd683a5d7c0ade169e600
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-04-29 22:30:02 +0900

    [BOJ] 암호 만들기 / 골드 5 / 백트래킹:

--- seungyeon/BAEKJOON/백트래킹/1759_암호 만들기.py
+++ seungyeon/BAEKJOON/백트래킹/1759_암호 만들기.py
@@ -1,119 +1,165 @@
-# # # 암호를 이루는 알파벳이 암호에서 증가하는 순서. abc (o) bac(x)
-# # # 최소 한개 모음 최소 두개 자음 
+# # # # 암호를 이루는 알파벳이 암호에서 증가하는 순서. abc (o) bac(x)
+# # # # 최소 한개 모음 최소 두개 자음 
+
+# # # import sys
+# # # from itertools import combinations
+
+# # # input=sys.stdin.readline
+
+# # # l,c=map(int,input().split())
+# # # arr=sorted(list(map(str,input().split())))
+# # # answer =[]
+
+# # # def backTracking(cnt,idx):
+# # #     if cnt == l:
+# # #         vo,co = 0,0
+        
+# # #         for i in range(l):
+# # #             if answer[i] in ['a','e','i','o','u']:
+# # #                 vo += 1
+# # #             else:
+# # #                 co += 1
+
+# # #         if vo >= 1 and co >= 2:
+# # #             print("".join(answer))
+# # #         return
+    
+# # #     for i in range(idx,c):
+# # #         answer.append(arr[i])
+# # #         backTracking(cnt+1, i+1)
+# # #         answer.pop()
+
+# # # backTracking(0,0)
+# # # # for i in combinations(arr,l):
+# # # #     print(''.join(i))
 
 # # import sys
-# # from itertools import combinations
-
 # # input=sys.stdin.readline
 
 # # l,c=map(int,input().split())
 # # arr=sorted(list(map(str,input().split())))
-# # answer =[]
+# # answer=[]
 
-# # def backTracking(cnt,idx):
-# #     if cnt == l:
-# #         vo,co = 0,0
-        
+# # # 정렬되어있는 모음1자음2
+
+# # def back(cnt,index):
+# #     if cnt == l: # 길이가 L이 되면 자음,모음 체크. 맞으면 print
+# #         vo,co=0,0
+
 # #         for i in range(l):
-# #             if answer[i] in ['a','e','i','o','u']:
+# #             if answer[i] in  ['a','e','i','o','u']:
 # #                 vo += 1
-# #             else:
+# #             else: 
 # #                 co += 1
 
-# #         if vo >= 1 and co >= 2:
+# #         if vo >= 1 and co >=2 :
 # #             print("".join(answer))
 # #         return
     
-# #     for i in range(idx,c):
+# #     for i in range(index,c):
 # #         answer.append(arr[i])
-# #         backTracking(cnt+1, i+1)
+# #         back(cnt+1,i+1)
 # #         answer.pop()
 
-# # backTracking(0,0)
-# # # for i in combinations(arr,l):
-# # #     print(''.join(i))
+# # back(0,0)
+
+
+
+
 
 # import sys
+
 # input=sys.stdin.readline
 
-# l,c=map(int,input().split())
-# arr=sorted(list(map(str,input().split())))
+# l,c=map(int,input().split(" "))
+# inputs=list(map(str,input().rstrip().split()))
+
+# arr = ["a","e","i","o","u"]
+
+# # 최소 1개 모음, 두개 자음
+
+# inputs.sort()
 # answer=[]
 
-# # 정렬되어있는 모음1자음2
+# visited=[False]*c
 
-# def back(cnt,index):
-#     if cnt == l: # 길이가 L이 되면 자음,모음 체크. 맞으면 print
-#         vo,co=0,0
-
-#         for i in range(l):
-#             if answer[i] in  ['a','e','i','o','u']:
-#                 vo += 1
-#             else: 
-#                 co += 1
-
-#         if vo >= 1 and co >=2 :
-#             print("".join(answer))
-#         return
+# def check(password):
+#     cnt,other = 0,0
     
+#     for i in password:
+#         if i in arr:
+#             cnt += 1
+#         else:
+#             other += 1
+
+#     # if cnt >= 2 or other >= 3:
+#     #     False
+#     # else:
+#     #     True
+
+#     return  cnt >= 1 and other >= 2
+
+# def dfs(index):
+
+
+#     if len(answer) == l:
+#         if check(answer):
+#             print(''.join(answer))
+#         return 
+
 #     for i in range(index,c):
-#         answer.append(arr[i])
-#         back(cnt+1,i+1)
-#         answer.pop()
+#             if not visited[i]:
+#                 answer.append(inputs[i])
+#                 dfs(i+1)
+#                 answer.pop()
+#                 visited[i] = False
 
-# back(0,0)
-
-
-
-
-
-import sys
-
-input=sys.stdin.readline
-
-l,c=map(int,input().split(" "))
-inputs=list(map(str,input().rstrip().split()))
-
-arr = ["a","e","i","o","u"]
-
-# 최소 1개 모음, 두개 자음
-
-inputs.sort()
-answer=[]
-
-visited=[False]*c
-
-def check(password):
-    cnt,other = 0,0
-    
-    for i in password:
-        if i in arr:
-            cnt += 1
-        else:
-            other += 1
-
-    # if cnt >= 2 or other >= 3:
-    #     False
-    # else:
-    #     True
-
-    return  cnt >= 1 and other >= 2
-
-def dfs(index):
-
-
-    if len(answer) == l:
-        if check(answer):
-            print(''.join(answer))
-        return 
-
-    for i in range(index,c):
-            if not visited[i]:
-                answer.append(inputs[i])
-                dfs(i+1)
-                answer.pop()
-                visited[i] = False
-
-dfs(0)
+# dfs(0)
 
             
+
+# 한개의 모음, 두개의 자음, 알파벳이 증가하는 순서
+
+l,c=map(int,input().split())
+arr=list(input().split())
+
+arr.sort()
+
+alpa=[]
+
+for i in range(c):
+    if arr[i] in ['a','e','i','o','u']:
+        alpa.append([arr[i],True])
+    else:
+        alpa.append([arr[i],False])
+
+def back(idx,answer,visited, a, b):
+
+    if len(answer) == l and a >= 1 and b >= 2:
+        print(''.join(answer))
+        return 
+        # return answer
+    
+    for i in range(idx,c):
+
+        if not visited[i]:
+
+            if alpa[i][1] :
+                a += 1
+            else:
+                b += 1
+
+            visited[i] = True
+            answer.append(alpa[i][0])
+            
+            back(i+1,answer,visited,a,b)
+
+            answer.pop()
+            if alpa[i][1] :
+                a -= 1
+            else:
+                b -= 1
+            visited[i] = False
+
+
+back(0,[],[False] * c,0,0)

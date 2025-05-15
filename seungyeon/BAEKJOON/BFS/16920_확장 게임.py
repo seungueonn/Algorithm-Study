f526b2df420af8ba1a47f871faf4d984e4a4dc50
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-05-15 17:12:51 +0900

    [BOJ] 확장 게임 / 골드 2 / BFS

---
+++ seungyeon/BAEKJOON/BFS/16920_확장 게임.py
@@ -0,0 +1,68 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+Y,X,P=map(int,input().split())
+s = [0]+list(map(int,input().split()))
+
+cnt=[0] * (P+1)
+que=[deque() for _ in range(P+1)]
+
+
+arr=[[0] * X for _ in range(Y)]
+for i in range(Y):
+    a = list(map(str,input().strip()))
+    for j in range(X):
+        if a[j] == '.':
+            arr[i][j] = 0
+        elif a[j] == '#':
+            arr[i][j] = -1
+        else:
+            arr[i][j] = int(a[j])
+            que[int(a[j])].append((j,i))
+            cnt[int(a[j])] += 1
+
+
+# i번째 플레이어가 s[i] 만큼 성을 확장함 
+
+
+dx=[-1,1,0,0]
+dy=[0,0,1,-1]
+
+
+
+def bfs():
+
+    is_move=True
+
+    while is_move:
+        is_move = False
+
+        for i in range(1,P+1):
+
+            if not que[i]:
+                continue
+            q = que[i]
+
+            for _ in range(s[i]): # z 만큼 움직임
+
+                if not q:
+                    break
+                for _ in range(len(q)):
+                    x,y=q.popleft()
+
+                    for j in range(4):
+                        nx = x + dx[j]
+                        ny= y + dy[j]
+
+                        if nx < 0 or ny < 0 or nx >= X or ny >= Y:
+                            continue
+                        
+                        if arr[ny][nx] == 0:
+                            arr[ny][nx] = i
+                            q.append((nx,ny))
+                            is_move=True
+                            cnt[i] += 1
+
+bfs()
+print(*cnt[1:])

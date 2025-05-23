57d3ad091237f96be6cab9acea1e544da602a5bc
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-05-23 15:27:22 +0900

    [BOJ] 적록색약 / 골드 5 / BFS

--- seungyeon/BAEKJOON/BFS/10026_적록색약.py
+++ seungyeon/BAEKJOON/BFS/10026_적록색약.py
@@ -1,181 +1,248 @@
-# # from collections import deque
+# # # from collections import deque
 
 
-# # def bfs(x,y):
+# # # def bfs(x,y):
 
-# #     que.append((x,y))
+# # #     que.append((x,y))
 
-# #     dx=[1,-1,0,0]
-# #     dy=[0,0,-1,1]
-# #     visited[x][y] = 1
+# # #     dx=[1,-1,0,0]
+# # #     dy=[0,0,-1,1]
+# # #     visited[x][y] = 1
 
-# #     while(que):
+# # #     while(que):
         
-# #         x,y = que.popleft()
+# # #         x,y = que.popleft()
 
-# #         for i in range(4):
-# #             nx = x + dx[i]
-# #             ny = y + dy[i]
+# # #         for i in range(4):
+# # #             nx = x + dx[i]
+# # #             ny = y + dy[i]
 
-# #             if nx < 0 or nx >= n or ny < 0 or ny >= n:
-# #                 continue
+# # #             if nx < 0 or nx >= n or ny < 0 or ny >= n:
+# # #                 continue
 
-# #             if arr[nx][ny] == arr[x][y] and not visited[nx][ny]:
-# #                 visited[nx][ny] = 1
-# #                 que.append((nx,ny))
+# # #             if arr[nx][ny] == arr[x][y] and not visited[nx][ny]:
+# # #                 visited[nx][ny] = 1
+# # #                 que.append((nx,ny))
             
 
 
+# # # n = int(input())
+# # # arr=[]
+# # # que = deque()
+
+# # # for i in range(n):
+# # #     arr.append(list(input().strip()))
+
+
+# # # visited=[[0]*n for _ in range(n)]
+# # # cnt1 = 0
+# # # for i in range(n):
+# # #     for j in range(n):
+# # #         if not visited[i][j]:
+# # #             bfs(i,j)
+# # #             cnt1 += 1
+
+# # # for i in range(n):
+# # #     for j in range(n):
+# # #         if arr[i][j] == 'G':
+# # #             arr[i][j] = 'R'
+
+# # # visited=[[0]*n for _ in range(n)]
+# # # cnt2 = 0
+# # # for i in range(n):
+# # #     for j in range(n):
+# # #         if not visited[i][j]:
+# # #             bfs(i,j)
+# # #             cnt2+=1
+
+# # # print(cnt1,cnt2)
+
+
+# # import sys
+# # from collections import deque
+# # input=sys.stdin.readline
+
 # # n = int(input())
 # # arr=[]
-# # que = deque()
-
 # # for i in range(n):
 # #     arr.append(list(input().strip()))
 
 
-# # visited=[[0]*n for _ in range(n)]
-# # cnt1 = 0
+# # # 2번째에는 R -> G 로 하고 구역 개수 세기
+# # dx=[-1,1,0,0]
+# # dy=[0,0,1,-1]
+
+# # def bfs(x,y):
+
+# #     que=deque()
+
+# #     que.append((x,y))
+# #     visited[x][y] = 1
+
+# #     while que:
+# #         x,y=que.popleft()
+
+# #         for i in range(4):
+# #             nx=x+dx[i]
+# #             ny=y+dy[i]
+
+# #             if nx < 0 or ny < 0 or nx >=n or ny >=n:
+# #                 continue
+
+# #             if arr[nx][ny] == arr[x][y] and visited[nx][ny] == 0:
+# #                 que.append((nx,ny))
+# #                 visited[nx][ny] = 1
+
+# # visited = [[0] * n for i in range(n)]
+# # cnt1,cnt2= 0,0
+
 # # for i in range(n):
 # #     for j in range(n):
 # #         if not visited[i][j]:
 # #             bfs(i,j)
 # #             cnt1 += 1
 
+
 # # for i in range(n):
 # #     for j in range(n):
-# #         if arr[i][j] == 'G':
-# #             arr[i][j] = 'R'
+# #         if arr[i][j] == "R":
+# #             arr[i][j] = "G"
 
-# # visited=[[0]*n for _ in range(n)]
-# # cnt2 = 0
+# # visited = [[0] * n for i in range(n)]
+
 # # for i in range(n):
 # #     for j in range(n):
 # #         if not visited[i][j]:
 # #             bfs(i,j)
-# #             cnt2+=1
+# #             cnt2 += 1
 
+   
 # # print(cnt1,cnt2)
 
-
+# # 적록색약이 아닌사람의 구역의 개수, 적록색약이 사람이 봤을 떄 구역의 개수 
 # import sys
-# from collections import deque
 # input=sys.stdin.readline
 
-# n = int(input())
+# n=int(input())
 # arr=[]
 # for i in range(n):
 #     arr.append(list(input().strip()))
 
 
-# # 2번째에는 R -> G 로 하고 구역 개수 세기
 # dx=[-1,1,0,0]
 # dy=[0,0,1,-1]
 
+# visited=[[False] * n for i in range(n)]
+
+# from collections import deque
+
 # def bfs(x,y):
-
 #     que=deque()
-
 #     que.append((x,y))
-#     visited[x][y] = 1
+
+#     visited[x][y] = True
 
 #     while que:
-#         x,y=que.popleft()
+#         x,y= que.popleft()
 
 #         for i in range(4):
 #             nx=x+dx[i]
 #             ny=y+dy[i]
 
-#             if nx < 0 or ny < 0 or nx >=n or ny >=n:
+#             if nx < 0 or ny < 0 or nx >=n or ny >= n :
 #                 continue
 
-#             if arr[nx][ny] == arr[x][y] and visited[nx][ny] == 0:
+#             if not visited[nx][ny] and arr[x][y] == arr[nx][ny]:
+#                 visited[nx][ny] = True
 #                 que.append((nx,ny))
-#                 visited[nx][ny] = 1
 
-# visited = [[0] * n for i in range(n)]
-# cnt1,cnt2= 0,0
-
+# cnt = 0
 # for i in range(n):
 #     for j in range(n):
 #         if not visited[i][j]:
 #             bfs(i,j)
-#             cnt1 += 1
+#             cnt += 1
 
-
+# # 적록색약
 # for i in range(n):
 #     for j in range(n):
+#         visited[i][j] = False
+
 #         if arr[i][j] == "R":
 #             arr[i][j] = "G"
 
-# visited = [[0] * n for i in range(n)]
-
+# cnt2 = 0
 # for i in range(n):
 #     for j in range(n):
 #         if not visited[i][j]:
 #             bfs(i,j)
 #             cnt2 += 1
 
-   
-# print(cnt1,cnt2)
+# print(cnt,cnt2)
 
-# 적록색약이 아닌사람의 구역의 개수, 적록색약이 사람이 봤을 떄 구역의 개수 
 import sys
+from collections import deque
+
 input=sys.stdin.readline
 
-n=int(input())
+n=int(input().strip())
 arr=[]
-for i in range(n):
-    arr.append(list(input().strip()))
+
+for _ in range(n):
+    arr.append((list(map(str,input().strip()))))
 
 
 dx=[-1,1,0,0]
 dy=[0,0,1,-1]
 
-visited=[[False] * n for i in range(n)]
+# 구역의 수 구하기 
+visited=[[0] * n for _ in range(n)]
 
-from collections import deque
+def bfs(x,y,cnt):
+    # 구역이 다름을 인지하고 갯수를 세야함 
+    visited[y][x] = cnt
 
-def bfs(x,y):
     que=deque()
     que.append((x,y))
 
-    visited[x][y] = True
-
     while que:
-        x,y= que.popleft()
+        x,y=que.popleft()
 
         for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            if nx < 0 or ny < 0 or nx >=n or ny >= n :
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
                 continue
 
-            if not visited[nx][ny] and arr[x][y] == arr[nx][ny]:
-                visited[nx][ny] = True
+            if arr[ny][nx] == arr[y][x] and visited[ny][nx] == 0:
+                visited[ny][nx] = visited[y][x]
                 que.append((nx,ny))
 
-cnt = 0
+cnt = 1
+answer=[]
+
 for i in range(n):
     for j in range(n):
-        if not visited[i][j]:
-            bfs(i,j)
+        if visited[i][j] == 0 :
+            bfs(j,i,cnt)
             cnt += 1
 
-# 적록색약
+answer.append(cnt-1)
+
 for i in range(n):
     for j in range(n):
-        visited[i][j] = False
+        if arr[i][j] == 'G':
+            arr[i][j] = 'R'
 
-        if arr[i][j] == "R":
-            arr[i][j] = "G"
-
-cnt2 = 0
+cnt = 1
+visited=[[0] * n for _ in range(n)]
 for i in range(n):
     for j in range(n):
-        if not visited[i][j]:
-            bfs(i,j)
-            cnt2 += 1
+        if visited[i][j] == 0 :
+            bfs(j,i,cnt)
+            cnt += 1
 
-print(cnt,cnt2)
+answer.append(cnt-1)
+
+print(*answer)

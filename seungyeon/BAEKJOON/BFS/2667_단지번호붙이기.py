3a32016798f6164c98fec2a527f0da5c3b781451
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-05-26 17:27:56 +0900

    [BOJ] 단지번호붙이기 / 실버 1 / BFS

--- seungyeon/BAEKJOON/BFS/2667_단지번호붙이기.py
+++ seungyeon/BAEKJOON/BFS/2667_단지번호붙이기.py
@@ -1,92 +1,148 @@
+# # import sys
+# # from collections import deque
+
+
+# # n = int(input())
+# # arr=[]
+# # for i in range(n):
+# #     arr.append(list(map(int,input())))
+
+# # answer = []
+# # dx=[1,-1,0,0]
+# # dy=[0,0,1,-1]
+
+# # def bfs(x,y):
+# #     que = deque()
+# #     cnt = 1
+
+# #     que.append((x,y))
+
+# #     while(que):
+# #         x,y = que.popleft()
+# #         arr[x][y] = 0
+
+# #         for i in range(4):
+# #             nx = x+dx[i]
+# #             ny = y+dy[i]
+
+# #             if 0<= nx < n and 0<= ny < n and arr[nx][ny] == 1:
+# #                 que.append((nx,ny))
+# #                 cnt += 1
+# #                 arr[nx][ny] = 0
+
+# #     answer.append(cnt)
+
+# # for i in range(n):
+# #     for j in range(n):
+# #         if arr[i][j] == 1:
+# #             bfs(i,j)
+
+# # print(len(answer))
+# # answer.sort()
+# # for i in answer:
+# #     print(i)
+
 # import sys
 # from collections import deque
 
+# input=sys.stdin.readline
 
 # n = int(input())
 # arr=[]
 # for i in range(n):
-#     arr.append(list(map(int,input())))
+#     arr.append(list(map(int,input().rstrip())))
 
-# answer = []
-# dx=[1,-1,0,0]
+
+# dx=[-1,1,0,0]
 # dy=[0,0,1,-1]
 
 # def bfs(x,y):
-#     que = deque()
-#     cnt = 1
+#     que=deque()
 
 #     que.append((x,y))
-
+#     arr[x][y] = 0
+#     cnt = 1
+    
+    
 #     while(que):
-#         x,y = que.popleft()
-#         arr[x][y] = 0
+#         x,y=que.popleft()
 
 #         for i in range(4):
-#             nx = x+dx[i]
-#             ny = y+dy[i]
+#             nx=x+dx[i]
+#             ny=y+dy[i]
 
-#             if 0<= nx < n and 0<= ny < n and arr[nx][ny] == 1:
+#             if nx < 0 or nx >= n or ny < 0 or ny >= n:
+#                 continue
+#             if arr[nx][ny] == 1:
+#                 arr[nx][ny] = 0
 #                 que.append((nx,ny))
 #                 cnt += 1
-#                 arr[nx][ny] = 0
+#     return cnt
 
-#     answer.append(cnt)
 
+# answer = []
 # for i in range(n):
 #     for j in range(n):
 #         if arr[i][j] == 1:
-#             bfs(i,j)
+#             answer.append(bfs(i,j))
 
-# print(len(answer))
 # answer.sort()
-# for i in answer:
-#     print(i)
+# print(len(answer))
+# for i in range(len(answer)):
+#     print(answer[i])
+
+# 단지수, 집의수 
 
 import sys
 from collections import deque
 
 input=sys.stdin.readline
 
-n = int(input())
+n=int(input().strip())
 arr=[]
-for i in range(n):
-    arr.append(list(map(int,input().rstrip())))
-
+for _ in range(n):
+    arr.append((list(map(int,input().strip()))))
+    
+visited=[[0] * n for _ in range(n)]
 
 dx=[-1,1,0,0]
 dy=[0,0,1,-1]
 
-def bfs(x,y):
-    que=deque()
+answer=[]
 
+def bfs(x,y):
+
+    global answer 
+
+    que=deque()
     que.append((x,y))
-    arr[x][y] = 0
+
     cnt = 1
-    
-    
-    while(que):
+    visited[y][x] = 1
+
+    while que:
         x,y=que.popleft()
 
         for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            if nx < 0 or ny < 0 or nx >= n or ny >= n :
                 continue
-            if arr[nx][ny] == 1:
-                arr[nx][ny] = 0
-                que.append((nx,ny))
+
+            if arr[ny][nx] == 1 and visited[ny][nx] == 0:
+                visited[ny][nx] = visited[y][x] + 1
                 cnt += 1
-    return cnt
+                que.append((nx,ny))
 
+    answer.append(cnt)
 
-answer = []
 for i in range(n):
     for j in range(n):
-        if arr[i][j] == 1:
-            answer.append(bfs(i,j))
+        if arr[i][j] == 1 and visited[i][j] == 0:
+            bfs(j,i)
 
-answer.sort()
 print(len(answer))
-for i in range(len(answer)):
-    print(answer[i])
+answer.sort()
+for k in answer:
+    print(k)

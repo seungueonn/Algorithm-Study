b5cd79cbabe6e3d79b74463f31dbc2c60c97843c
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-06-04 12:29:00 +0900

    [BOJ] 퇴사2 / 골드 5 / DP

--- seungyeon/BAEKJOON/DP/15496_퇴사2.py
+++ seungyeon/BAEKJOON/DP/15496_퇴사2.py
@@ -1,17 +1,37 @@
+# import sys
+# input=sys.stdin.readline
+
+# n = int(input())
+
+# arr=[]
+# for i in range(n):
+#     arr.append(list(map(int,input().split(" "))))
+
+# dp=[0 for i in range(n+1)]
+
+# for i in range(n-1,-1,-1):
+#     if i + arr[i][0] > n:
+#         dp[i] = dp[i+1]
+#     else :
+#         dp[i] = max(dp[i+1],arr[i][1] + dp[i+arr[i][0]]) 
+# print(dp[0])
+
+
+# 상담을 해서 얻을 수 있는 최대 이익
+
+
 import sys
 input=sys.stdin.readline
 
-n = int(input())
+n=int(input().strip())
 
-arr=[]
-for i in range(n):
-    arr.append(list(map(int,input().split(" "))))
+dp = [0] * (n+1)
+for i in range(1,n+1):
+    t,p =map(int,input().split())
 
-dp=[0 for i in range(n+1)]
+    dp[i] = max(dp[i-1],dp[i])
 
-for i in range(n-1,-1,-1):
-    if i + arr[i][0] > n:
-        dp[i] = dp[i+1]
-    else :
-        dp[i] = max(dp[i+1],arr[i][1] + dp[i+arr[i][0]]) 
-print(dp[0])
+    if i + t <= n+1:
+        dp[i+t-1] = max(dp[i-1]+p,dp[i+t-1])
+
+print(dp[-1])
